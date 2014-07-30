ca008686ed501af4a846d5ba199bcca535c10556
Mr. Spread <Mr-Spread@users.noreply.github.com>
2014-07-30 10:50:59 +0400

    Change to SpreadCoin

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x4f, 0x3c, 0x5c, 0xbb };
 
 CMessageHeader::CMessageHeader()
 {
